bdb809b5edf5fbfea9531c96857112571ac58bed
Volodymyr Kulivatskyy <kulivackijvoloda@gmail.com>
2025-10-15 09:59:07 +0300

    fix/[L] add vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // <-- саме цю папку CircleCI шукає
+    outDir: 'dist',
     sourcemap: false,
   },
   server: {
